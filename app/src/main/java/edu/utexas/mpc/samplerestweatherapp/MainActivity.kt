6f11a21beba818d7492175fd5c66d236ad577b84
Matt Viteri <mxviteri@gmail.com>
2019-11-23 13:52:13 -0600

    finalized app

--- app/src/main/java/edu/utexas/mpc/samplerestweatherapp/MainActivity.kt
+++ app/src/main/java/edu/utexas/mpc/samplerestweatherapp/MainActivity.kt
@@ -1,35 +1,30 @@
 package edu.utexas.mpc.samplerestweatherapp
 
-import android.app.Activity
-import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
 import android.icu.text.SimpleDateFormat
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
-import android.os.SystemClock.sleep
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.provider.Settings
 import android.support.annotation.RequiresApi
+import android.widget.Toast
 import com.android.volley.RequestQueue
-import com.android.volley.toolbox.ImageRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_main.*
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended
 import org.eclipse.paho.client.mqttv3.MqttMessage
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
-import kotlin.collections.HashMap
-import kotlin.text.Typography.tm
 
 class MainActivity : AppCompatActivity() {
 
@@ -183,6 +178,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendWeather(){
+        val networkCheck = Toast.makeText(this@MainActivity,  "Make sure you are on the Raspberry Pi network", Toast.LENGTH_LONG)
+        networkCheck.show()
+
         val message = MqttMessage()
         message.payload = (weatherData.toString()).toByteArray()
         mqttAndroidClient.publish(publishTopic, message)
@@ -196,8 +194,7 @@ class MainActivity : AppCompatActivity() {
 
     fun launchWifiSettings(){
         val intent = Intent(android.provider.Settings.ACTION_WIFI_SETTINGS)
-//        startActivity(Intent(android.provider.Settings.ACTION_WIFI_SETTINGS))
-        startActivity(Intent.createChooser(intent, "dialogTitle"));
+        startActivity(Intent.createChooser(intent, "Wifi Settings"));
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
